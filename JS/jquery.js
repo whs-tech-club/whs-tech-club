539a13ee6f5123ea98036f44c2ae7dd7dd82ee8b
cpabbot <cpabbot1@gmail.com>
2016-02-24 16:01:02 -0500

    updated idea page, general

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+    var width = $(window).width();
+    resize();
+    $(window).resize(function(){
+       resize();
+    });
+    
+    function resize() {
+        if($(this).width() != width){
+           width = $(this).width();
+           if(width < 540) {
+               //alert(width);
+               //$("a").hide();
+           }
+           /*else if(width > 540) {
+               //alert(width);
+               $("li").show();
+           }*/
+       }
+    }
+    
     jQuery("#heading").fitText(1.5, { minFontSize: '30px', maxFontSize: '60px' });
 
     var stickyNavTop = $('.nav').offset().top;
@@ -8,7 +28,7 @@ $(document).ready(function() {
         var scrollTop = $(window).scrollTop();
       
         if (scrollTop > stickyNavTop) { 
-            $('.nav').addClass('sticky');
+            $('ul.nav').addClass('sticky');
         }
         else {
             $('.nav').removeClass('sticky'); 
